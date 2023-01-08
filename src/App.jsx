6f736fd4513992c1f89fac9211662652e7c1ee3b
noracamacho <ingnorac@gmail.com>
2023-01-08 15:10:14 -0600

    Adding autocomplete in search input

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,13 @@ import { useState, useEffect } from 'react'
 import ResidentsInfo from './components/ResidentsInfo';
 import Pagination from './components/Pagination';
 import Footer from './components/Footer';
-// import LiveSearch from './components/LiveSearch';
 import LocationInfo from './components/LocationInfo';
 import './App.css'
-import Search from 'antd/es/transfer/search';
-import SearchBar from './components/SearchBar';
-import names from './assets/data/names';
 
 function App() {
 
   const [data, setData] = useState({})
   const [locationId, setLocationId] = useState(Math.ceil(Math.random()*126));
-  // const [idValue, setIdValue] = useState();
    //* ----------------------------------------------------------------------
   //? LOCATION INFORMATION
   const [locationName, setLocationName] = useState('');
@@ -23,23 +18,18 @@ function App() {
    //* ----------------------------------------------------------------------
 //? Residents cards
   const [residents, setResidents] = useState([]);
-  // const [placeholderText, setPlaceholderText] = useState('Type a location Id')
    //* ----------------------------------------------------------------------
   //? PAGINATION
-  // const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
   //* ----------------------------------------------------------------------
   //? LIVE SEARCH
   const [locations, setLocations] = useState([]);
   const [locationMatch, setLocationMatch] = useState([]);
-  // const [data, setData] = useState({})
-  // const [selection, setSelection] = useState('Enter location name');
   const [enteredWord, setEnteredWord] = useState('');
    //* ----------------------------------------------------------------------
 
   useEffect(() => {
-    // setLoading(true);
     axios.get(`https://rickandmortyapi.com/api/location/${locationId}`)
         .then((response) => setData(response.data))
         // .then(console.log('data', data))
@@ -49,12 +39,10 @@ function App() {
 
   useEffect(() => {
     if (data) {
-      // console.log(data)
       setLocationName(data.name);
       setLocationType(data.type);
       setLocationDimension(data.dimension);
       setResidents(data.residents);
-      // setLoading(false);
     }
   }, [data]);
 
@@ -81,8 +69,6 @@ function App() {
         setLocationMatch(matches);
     }
   };
-  //* ----------------------------------------------------------------------
-
 
   //* ----------------------------------------------------------------------
   //? PAGINATION
@@ -101,7 +87,6 @@ function App() {
         <div className="header__img"></div>
 
         {/* <SearchBar placeHolder={'Type a location name'} data={names}/> */}
-        {/* LIVESEARCH */}
         {/* <LiveSearch /> */}
         <div className="results">
             {/* ++++++++++++++++++ */}
@@ -124,6 +109,7 @@ function App() {
 
           {/* Location information */}
           <LocationInfo name={locationName} type={locationType} dimension={locationDimension} residents={residents?.length} />
+          
           {/* Residents cards  */}
           <div className="residents__container">
             {currentPosts?.map(resident => (
@@ -133,9 +119,7 @@ function App() {
       </div>
       <Pagination postsPerPage={postsPerPage} totalPosts={residents?.length} paginate={paginate} />
       <Footer />
-      
     </div>
   )
 }
-
 export default App
